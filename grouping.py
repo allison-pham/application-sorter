388e9edc7caac4d42054a99b30ffdb8edb1288ff
Allison <138368973+allison-pham@users.noreply.github.com>
2024-03-05 16:25:53 -0800

    Call all functions in main.py

--- grouping.py
+++ grouping.py
@@ -3,6 +3,11 @@ from collections import Counter
 def count_subjects(df):
     return Counter(df['Subject(s) of interest for research (Please check the above document attached)'])
 
+    # subject_counter = Counter(list(df['Subject(s) of interest for research (Please check the above document attached)']))
+
+    # for subject, count in subject_counter.items():
+    # print(f"Subject: {subject}\tNumber of Respondents: {count}")
+
 def create_groups(subject_counter, max_members = 4):
     groups = []
     for subject, count in subject_counter.items():
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-import pandas as pandas
+from file_handler import read_file, save_file
+from grouping import count_subjects, create_groups
+import pandas as pd
 
+# User input
+file_path = input("Enter the file path to your CSV file: ")
+application_df = read_file(file_path)
 
-# Archive 1
+# Counter & create groups
+subject_counter = count_subjects(application_df)
+groups = create_groups(subject_counter)
+
+sorted_groups = pd.DataFrame(groups)
+save_file(sorted_groups, 'sorted_groups.csv')
+
+# Archive
 # df = pd.read_csv('research_program_responses.csv') # Adjust file path
 
 # subject_counter = df['Subject(s) of interest for research (Please check the above document attached)'].value_counts()
 
 # subject_counter_df = subject_counts.reset_index()
 # subject_counter_df.columns['Subject(s) of interest for research (Please check the above document attached)']
-
-
-# Archive 2
-# # User input
-# file_path = input("Enter the file path to the file. Example file type: .csv")
-# application_df = pd.read_csv(file_path)
-
-# # Counter
-# subject_counter = Counter(list(df['Subject(s) of interest for research (Please check the above document attached)']))
-
-# # Count each subject
-# for subject, count in subject_counter.items():
-#     print(f"Subject: {subject}\tNumber of Respondents: {count}")
